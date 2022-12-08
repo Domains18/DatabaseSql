ee7bfcf200e49ddf5f8519a4e60340fe92df70cf
Domains18 <gibsonsgibson88@gmail.com>
2022-12-08 20:40:51 +0300

    Services, Error, Home and Rooms

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import Banner from '../Components/Banner'
+import  { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <Hero/>
+    <Hero>
+      <Banner title="Luxurios rooms " subtitle="deluxe rooms at $200">
+        <Link to="/rooms" className='btn-primary'>
+          Our Rooms
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
 
--- src/Pages/Rooms.js
+++ src/Pages/Rooms.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import Banner from '../Components/Banner'
+import { Link } from 'react-router-dom'
 const Rooms = () => {
   return (
-    <Hero hero='roomsHero' />
+    <Hero hero='roomsHero'>
+      <Banner title="Our Rooms">
+        <Link to='/rooms' className='btn-primary'>
+          return Home
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
 
