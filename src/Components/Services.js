90324cda0adfa30dad4b89746fc9c9f5ad1eabd3
Domains18 <gibsonsgibson88@gmail.com>
2022-12-12 06:17:19 +0300

    services

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
-
+import Title from './Title'
+import { FaCocktail, FaHiking,FaShuttleVan, FaBeer } from 'react-icons/fa'
 export default class Services extends Component {
+  state = {
+    services:[
+      {
+        icon: <FaCocktail/>,
+        title:"Free Cocktails",
+        info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, suscipit?"
+      },
+      {
+        icon: <FaHiking/>,
+        title: "Endless Hikng",
+        info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, suscipit?"
+      },
+      {
+        icon :<FaShuttleVan/>,
+        title: "Free Shuttle",
+        info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, suscipit?"
+      },
+      {
+        icon: <FaBeer />,
+        title: "strongest beer",
+        info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, suscipit?"
+      }
+    ]
+  }
   render() {
     return (
       <div>
-        
+        <Title title="services"/>
       </div>
     )
   }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import Banner from '../Components/Banner'
-import  { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Services from '../Components/Services';
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="Luxurios rooms " subtitle="deluxe rooms at $200">
-        <Link to="/rooms" className='btn-primary'>
-          Our Rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title="Luxurios rooms " subtitle="deluxe rooms at $200">
+          <Link to="/rooms" className='btn-primary'>
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services/>
+    </>
   )
 }
 
