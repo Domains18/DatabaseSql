b6bc219d95c47f5bb799c8703cca2fff607d39fa
Domains18 <gibsonsgibson88@gmail.com>
2022-12-13 13:47:22 +0300

    featured rooms

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import defaultImg from '../images/room-1.jpeg';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 export default function Room({ room }) {
-
     const { name, slug, images, price } = room;
     return (
 
@@ -14,8 +14,19 @@ export default function Room({ room }) {
                     <p>per night</p>
                 </div>
                 <Link to={`/rooms/${slug}`} className='btn-primary room-link'>
+                    Features
                 </Link>
             </div>
+            <p className='room-info'>{name}</p>
         </article>
     )
+}
+
+Room.propTypes = { 
+    room: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        images: PropTypes.arrayOf(PropTypes.string).isRequired,
+        price: PropTypes.number.isRequired,
+    })
 }
