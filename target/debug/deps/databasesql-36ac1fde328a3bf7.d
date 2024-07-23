8fd3d547416eb20713e9e682efd5a55fe3be20b0
Domains18 <gibsonsgibson88@gmail.com>
2024-07-23 08:55:18 +0300

    Remove unused components and files

--- target/debug/deps/databasesql-36ac1fde328a3bf7.d
+++
@@ -1,5 +0,0 @@
-/home/alpha/Documents/Github/Rust/DatabaseSql/target/debug/deps/libdatabasesql-36ac1fde328a3bf7.rmeta: src/main.rs
-
-/home/alpha/Documents/Github/Rust/DatabaseSql/target/debug/deps/databasesql-36ac1fde328a3bf7.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/databasesql-65ec568dfd70fae5.d
+++
@@ -1,5 +0,0 @@
-/home/alpha/Documents/Github/Rust/DatabaseSql/target/debug/deps/libdatabasesql-65ec568dfd70fae5.rmeta: src/main.rs
-
-/home/alpha/Documents/Github/Rust/DatabaseSql/target/debug/deps/databasesql-65ec568dfd70fae5.d: src/main.rs
-
-src/main.rs:
