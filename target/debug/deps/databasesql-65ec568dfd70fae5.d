05cb6e18e76686112f273b5835429e3515024ace
Domains18 <gibsonsgibson88@gmail.com>
2024-07-23 08:52:36 +0300

    codebase

---
+++ target/debug/deps/databasesql-65ec568dfd70fae5.d
@@ -0,0 +1,5 @@
+/home/alpha/Documents/Github/Rust/DatabaseSql/target/debug/deps/libdatabasesql-65ec568dfd70fae5.rmeta: src/main.rs
+
+/home/alpha/Documents/Github/Rust/DatabaseSql/target/debug/deps/databasesql-65ec568dfd70fae5.d: src/main.rs
+
+src/main.rs:
